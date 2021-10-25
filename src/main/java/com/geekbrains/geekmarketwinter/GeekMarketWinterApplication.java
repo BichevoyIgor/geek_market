b0adf526452079670cac56b3a7449032a4757fdf
BichevoyIgor <igoreek@mail.ru>
2021-10-25 14:19:54 +0300

    hw

--- src/main/java/com/geekbrains/geekmarketwinter/GeekMarketWinterApplication.java
+++ src/main/java/com/geekbrains/geekmarketwinter/GeekMarketWinterApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+
 @ComponentScan
 @EnableAutoConfiguration
 public class GeekMarketWinterApplication {
